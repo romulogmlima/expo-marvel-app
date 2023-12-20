82cd745bbe51238b4097d18bbf9286e578a2e16a
Rômulo Gadelha <romulogadelhaml@gmail.com>
2023-12-20 15:02:56 -0300

    feat(components): create the touchable-opacity-box component

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export * from "./Box/Box";
+export * from "./CharacterCard/CharacterCard";
+export * from "./ComicCard/ComicCard";
+export * from "./Screen/Screen";
+export * from "./Text/Text";
+export * from "./TouchableOpacityBox/TouchableOpacityBox";
