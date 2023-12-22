1759a1d802442d581c484268e6194bec7e565fff
Rômulo Gadelha <romulogadelhaml@gmail.com>
2023-12-22 10:12:52 -0300

    chore(deps): install axios-mock-adapter

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import { Router } from "@/routes/Routes";
@@ -6,6 +7,7 @@ export default function App() {
   return (
     <SafeAreaProvider>
       <Router />
+      <StatusBar style="auto" />
     </SafeAreaProvider>
   );
 }
